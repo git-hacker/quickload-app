7aa2fc6714a60125c9eee649941c0af55dff6fe6
Christopher Chen <chenchristophert@gmail.com>
2018-09-09 11:46:14 +0800

    it works
    convert weight to integer

--- pages/results/results.js
+++ pages/results/results.js
@@ -26,7 +26,7 @@ Page({
         "Origin": options.origin,
         "TruckType": {
           "Length": options.length * 100,
-          "Weight": options.weight,
+          "Weight": options.weight * 1,
           "Type": options.truckType
         }
       }
@@ -46,9 +46,21 @@ Page({
     console.log(body)
     wx.request({
       url: host + 'find-shipment',
-      method: 'post',
-      header: { 'Content-Type': 'application/json' },
-      data: body,
+      method: 'POST',
+      header: {'Content-Type': 'application/json' },
+      data: {
+
+        "Name": "Driver A",
+        "License": "232393873",
+        "Origin": "成都",
+
+        "TruckType": {
+          "Length": 50,
+          "Weight": 35,
+
+          "Type": "Refrigerator"
+        }
+      },
       success: res => {
         console.log('success', res)
       }
